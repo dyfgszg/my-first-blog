6cb8c363acf477a0fc101cd678a4df7e751c519f
dyfgs_zg <dyfgs_zg@cttsc.com>
2017-02-10 00:23:40 +0800

    test git
    Signed-off-by: dyfgs_zg <dyfgs_zg@cttsc.com>

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -5,6 +5,8 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+
+test git
 """
 
 import os
